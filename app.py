6c0ec5fc34ee4f591eeeaed19641c2f6425e6038
bruno <bruno.teixeira@dafiti.com.br>
2021-12-02 10:12:36 -0300

    Postgresql + edit route

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/flask'
 
 db = SQLAlchemy(app)
 
 class Product(db.Model):
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True, unique=True)
     name = db.Column(db.String(20), unique=True)
-    brand = db.Column(db.String(20), unique=True)
-    price = db.Column(db.Float) 
+    brand = db.Column(db.String(20))
+    price = db.Column(db.Float)
     
     def __init__ (self, name, brand, price):
         self.name = name
@@ -38,6 +38,19 @@ def delete(id):
     db.session.delete(product)
     db.session.commit()
     return redirect(url_for('index'))
+
+@app.route('/edit/<int:id>', methods=['POST','GET'])
+def edit(id):
+    product = Product.query.get(id)
+    if request.method == 'POST':
+        product.name = request.form['name']
+        product.brand = request.form['brand']
+        product.price = request.form['price']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', product=product)
+
+
 
 if __name__ == '__main__':
     db.create_all()
