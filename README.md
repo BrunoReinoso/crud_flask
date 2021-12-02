c0574881e94780a27c27e17ad5a48236b458885f
bruno <bruno.teixeira@dafiti.com.br>
2021-12-02 10:36:50 -0300

    Requirements, Readme and other files

--- README.md
+++ README.md
@@ -1,8 +1,46 @@
-# crud_flask
+# Crud Flask
+
+## Description 
     A simple CRUD implementation with flask microframework.
     
-## How to run
+## Architecture
+  - A web application using Flask (python language).
+  - A postgresql database.
+
+## Development
+This project was developed on Linux Ubuntu 21.04. 
+
+Then, we recommend installing pyenv and pyenv-virtualenv, install python 3.8.0 on it and then create a pyenv virtualenv with this python version.
+
+Alternatively, in case you need, there are also commands in the Makefile to build and run the containers for the app.
+
+
+## Installation
+
+### Clone the project
+
+```bash
+  git clone https://github.com/BrunoReinoso/crud_flask
+```
+
+### Activate the virtualenv and install the dependencies (if you want to use Docker, skip this step)
+
+```bash
+  make requirements-pip
+```
+
+### Up the container with database
+
+```bash
+  make docker-compose-up
+```
+
+### How to run
 
 ```bash
     python app.py
 ```
+
+# Author
+
+- [@BrunoReinoso](https://github.com/BrunoReinoso/)
--- app.py
+++ app.py
@@ -1,57 +1,64 @@
 from itertools import product
-from flask import Flask, render_template, request, url_for, redirect
+
+from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/flask'
+app = Flask(__name__, template_folder="templates")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:admin@localhost/flask"
 
 db = SQLAlchemy(app)
 
+
 class Product(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True, unique=True)
+    id = db.Column("id", db.Integer, primary_key=True, autoincrement=True, unique=True)
     name = db.Column(db.String(20), unique=True)
     brand = db.Column(db.String(20))
     price = db.Column(db.Float)
-    
-    def __init__ (self, name, brand, price):
+
+    def __init__(self, name, brand, price):
         self.name = name
         self.brand = brand
         self.price = price
 
-@app.route('/')
+
+@app.route("/")
 def index():
     products = Product.query.all()
-    return render_template('index.html', products=products)
+    return render_template("index.html", products=products)
 
-@app.route('/add', methods=['GET','POST'])
+
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    if request.method == 'POST':
-        product = Product(request.form['name'], request.form['brand'], request.form['price'])
+    if request.method == "POST":
+        product = Product(
+            request.form["name"], request.form["brand"], request.form["price"]
+        )
         db.session.add(product)
         db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('add.html')
+        return redirect(url_for("index"))
+    return render_template("add.html")
 
-@app.route('/delete/<int:id>')
+
+@app.route("/delete/<int:id>")
 def delete(id):
     product = Product.query.get(id)
     db.session.delete(product)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/edit/<int:id>', methods=['POST','GET'])
+
+@app.route("/edit/<int:id>", methods=["POST", "GET"])
 def edit(id):
     product = Product.query.get(id)
-    if request.method == 'POST':
-        product.name = request.form['name']
-        product.brand = request.form['brand']
-        product.price = request.form['price']
+    if request.method == "POST":
+        product.name = request.form["name"]
+        product.brand = request.form["brand"]
+        product.price = request.form["price"]
         db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('edit.html', product=product)
+        return redirect(url_for("index"))
+    return render_template("edit.html", product=product)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
